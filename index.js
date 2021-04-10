5265d4bd876b223e66c49f4502b391cf213a3032
Neha Kumari <nehakumari1461@gmail.com>
2021-04-10 20:57:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const socketio = require('socket.io');
+const express = require('express');
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: "*"}});
+app.set("view engine", "ejs")
+app.get("/home", (req, res) => {
+res.render("home");
+});
+
+server.listen(3001, () => {
+console.log("server running...");
+});
+io.on('connection', (socket) =>{
+console.log("user:" + socket.id);
+
+socket.on("message", (data) =>{
+ socket.broadcast.emit('message',data)
+});
+});
